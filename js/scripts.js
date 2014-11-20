8b27fce7aabb0e8d09f62f648b52193b849826b3
Randy Leighton <randyleighton@yahoo.com>
2014-11-20 00:39:19 -0800

    experimenting with 1.3 change to global controller

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,19 @@
-var app = angular.module('app',['ngMessages']);
+var appmsg = angular.module('app',['ngMessages']);
 
 app.controller('MainCtrl',function MainCtrl() {
   'use strict';
   var vm = this;
   vm.angularVersion = angular.version.full;
 });
+
+angular.module('app', []).config(function($controllerProvider) {
+$controllerProvider.allowGlobals();
+
+// This is 1.2 no longer this way in 1.3
+function FirstCtrl($scope) {
+  $scope.data = {message: "panel"}; 
+}
+
+});
+
+
