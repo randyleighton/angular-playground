affd749b296035a4da7de3dba46742c86aea9ad8
Randy Leighton <randyleighton@yahoo.com>
2014-11-20 08:34:12 -0800

    v1.2 code sharing between global controller

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,12 @@
-var appmsg = angular.module('app',['ngMessages']);
+var myApp = angular.module('myApp', []);
+myApp.factory('Data', function() {
+    return {message: "service data"}
+})
 
-app.controller('MainCtrl',function MainCtrl() {
-  'use strict';
-  var vm = this;
-  vm.angularVersion = angular.version.full;
-});
-
-angular.module('app', []).config(function($controllerProvider) {
-$controllerProvider.allowGlobals();
-
-// This is 1.2 no longer this way in 1.3
-function FirstCtrl($scope) {
-  $scope.data = {message: "panel"}; 
+function FirstCtrl($scope, Data){
+  $scope.data = Data;
 }
 
-});
-
-
+function SecondCtrl($scope, Data){
+  $scope.data = Data;
+}
